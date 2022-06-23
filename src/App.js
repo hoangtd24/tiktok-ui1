03808da0328f5104e787ce0ef7d16f4decfc74f7
tonghoang <tongduchoang@gmail.com>
2022-06-23 10:26:54 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Following from './pages/Following';
+import Home from './pages/Home';
+import Live from './pages/Live';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path = "/" element = {<Home />}></Route>
+                    <Route path = "/following" element = {<Following />}></Route>
+                    <Route path = "/live" element = {<Live />}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
