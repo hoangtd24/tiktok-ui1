8da3fd0ef0ad36a01b1c29ac1f6305cad2839d03
Tong Hoang <hoangtd24100@gmail.com>
2022-07-21 17:08:38 +0700

    xay dung logic phan Header #3

--- src/layouts/components/Search/index.js
+++ src/layouts/components/Search/index.js
@@ -9,6 +9,7 @@ import styles from './Search.module.scss';
 import { Wrapper as PoperWrapper } from '~/Poper';
 import AccountItem from '~/components/AccountItem';
 import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
 
 
 const cx = classNames.bind(styles);
@@ -21,6 +22,7 @@ function Search () {
     const [showResult,setShowResult] = useState(true)
     const [loading, setLoading] = useState(false)
 
+    const debounced = useDebounce(searchValue,500)
 
     useEffect(() => {
         if(!searchValue.trim()){
@@ -28,14 +30,14 @@ function Search () {
             return
         }
         setLoading(true)
-        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(searchValue)}&type=less`)
+        fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
         .then(res => res.json())
         .then(res => {
             setSearchResult(res.data)
             setLoading(false)
         })
         .catch(() => setLoading(false))
-    },[searchValue])
+    },[debounced])
     const handleClear = () => {
         setSearchValue('')
         setShowResult(false)
